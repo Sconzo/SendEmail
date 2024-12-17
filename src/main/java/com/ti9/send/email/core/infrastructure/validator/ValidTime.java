11a8b45cb37470271556b8cf6bea948e599a4173
Rodrigo Polydoro <rspolydoro@gmail.com>
2024-12-17 19:35:39 -0300

    feat(created annotation validator)

---
+++ src/main/java/com/ti9/send/email/core/infrastructure/validator/ValidTime.java
@@ -0,0 +1,19 @@
+package com.ti9.send.email.core.infrastructure.validator;
+
+import com.ti9.send.email.core.application.validator.TimeValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = TimeValidator.class)
+@Target({ ElementType.TYPE_USE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidTime {
+    String message() default "Invalid time value. Allowed format: HH:mm, max value: 23:55";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
