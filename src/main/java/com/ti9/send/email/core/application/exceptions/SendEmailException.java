8507be6dfee73dc982a77636283f629d514cfa04
Rafael Ferreira <rafaelferreira.dev@gmail.com>
2025-01-16 15:35:14 -0300

    feat(Exceptions): implemented custom exceptions.

--- src/main/java/com/ti9/send/email/core/application/exceptions/SendEmailException.java
+++ src/main/java/com/ti9/send/email/core/application/exceptions/SendEmailException.java
@@ -2,15 +2,15 @@ package com.ti9.send.email.core.application.exceptions;
 
 import java.io.Serial;
 
-public class InvalidInputException extends RuntimeException {
+public class SendEmailException extends RuntimeException {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    public InvalidInputException(String message) {
+    public SendEmailException(String message) {
         super(message);
     }
 
-    public InvalidInputException(String message, Throwable cause) {
+    public SendEmailException(String message, Throwable cause) {
         super(message, cause);
     }
 }
--- src/main/java/com/ti9/send/email/core/infrastructure/adapter/out/sender/Sender.java
+++ src/main/java/com/ti9/send/email/core/infrastructure/adapter/out/sender/Sender.java
@@ -1,8 +1,6 @@
 package com.ti9.send.email.core.infrastructure.adapter.out.sender;
 
-import jakarta.mail.MessagingException;
-
 public interface Sender<T> {
 
-    void send(T t) throws MessagingException;
+    void send(T t);
 }
