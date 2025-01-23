391558110085df8c099add78c1871ca134740701
Rodrigo Polydoro <rspolydoro@gmail.com>
2025-01-23 20:26:22 -0300

    adjusments

--- src/main/java/com/ti9/send/email/core/infrastructure/config/MailConfig.java
+++ src/main/java/com/ti9/send/email/core/infrastructure/config/MailConfig.java
@@ -30,13 +30,29 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        
+
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.starttls.required", "true");
         props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+        return mailSender;
+    }
+
+    @Bean(name = "smtpOutlookMailSender")
+    public JavaMailSender smtpOutlookMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.office365.com");
+        mailSender.setPort(587);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.office365.com");
 
         return mailSender;
     }
